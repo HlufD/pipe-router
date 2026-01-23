94c5c766a5ff9c49f201de5d1fcd16d7166d9260
Hluf Abebe <hlufabebe2015@gmail.com>
2026-01-23 18:51:04 +0300

    add separte methods to register segments

--- src/utils/Trie-Route.ts
+++ src/utils/Trie-Route.ts
@@ -7,6 +7,10 @@ const enum SEGMENT_TYPE {
   "wildcard" = "wildcard",
 }
 
+interface Options {
+  ignoreDuplicateSlashes?: boolean;
+}
+
 class Node {
   public handlers: Map<HTTP_METHODS, RouteHandler[]>;
   public staticNodes: Map<string, Node>;
@@ -25,47 +29,72 @@ class Node {
 
 class RouteNode {
   private root = new Node();
+  public config: Options = {};
+  constructor(config: Options) {
+    this.config = config;
+  }
 
-  on(url: string, method: HTTP_METHODS, ...handlers: RouteHandler[]) {
+  on(url: string, method: HTTP_METHODS, handlers: RouteHandler[]) {
     let currNode = this.root;
     const segments = this.normalizeUrl(url);
-
     console.log(segments);
 
     for (const segment of segments) {
-      const segmentType = this.identifySegmentType(segment);
-      console.log(segment, segmentType);
-
-      switch (segmentType) {
+      switch (this.identifySegmentType(segment)) {
         case SEGMENT_TYPE.static:
-          currNode = this.registerStaticNode(segment, currNode, ...handlers);
+          currNode = this.registerStaticNode(segment, currNode);
+          break;
+
         case SEGMENT_TYPE.parametric:
+          currNode = this.registerParametricNode(segment, currNode);
+          break;
 
         case SEGMENT_TYPE.wildcard:
+          currNode = this.registerWildcardSegment(segment, currNode);
+          break;
+
+        default:
+          break;
       }
     }
+
+    this.registerHandlers(method, handlers, currNode, segments);
   }
 
-  private registerStaticNode(
-    segment: string,
-    currNode: Node,
-    ...handler: RouteHandler[]
-  ): Node {
+  private registerStaticNode(segment: string, currNode: Node): Node {
     if (!currNode.staticNodes.has(segment)) {
       currNode.staticNodes.set(segment, new Node());
     }
 
     currNode = currNode.staticNodes.get(segment)!;
+    return currNode;
+  }
 
+  private registerParametricNode(segment: string, currNode: Node): Node {
+    const parameterName = this.getParamName(segment);
+
+    if (!currNode.parametricNode) {
+      currNode.parametricNode = new Node();
+      currNode.parametricNode.parameter = parameterName;
+    }
+
+    currNode = currNode.parametricNode;
+    return currNode;
+  }
+
+  private registerWildcardSegment(segment: string, currNode: Node): Node {
+    currNode.wildCardNode = new Node();
+    currNode = currNode.wildCardNode;
     return currNode;
   }
 
   private normalizeUrl(url: string) {
-    if (url != "/") {
+    if (this.config.ignoreDuplicateSlashes) {
+      return url.split("/").filter(Boolean);
+    } else {
       url = url.replace(/\/+$/, "");
+      return url.split("/");
     }
-
-    return url.split("/");
   }
 
   private identifySegmentType(segment: string): SEGMENT_TYPE {
@@ -79,10 +108,37 @@ class RouteNode {
 
     return SEGMENT_TYPE.static;
   }
+
+  private registerHandlers(
+    method: HTTP_METHODS,
+    handlers: RouteHandler[],
+    currNode: Node,
+    segments: string[],
+  ) {
+    if (
+      this.config.ignoreDuplicateSlashes &&
+      this.isDuplicatePath(method, currNode)
+    ) {
+      throw new Error(`Duplicate routes at: /${segments.join("/")}`);
+    }
+
+    currNode.handlers.set(method, handlers);
+  }
+
+  private isDuplicatePath(method: HTTP_METHODS, currNode: Node) {
+    return currNode.handlers.get(method) ? true : false;
+  }
+
+  private getParamName(segment: string) {
+    return segment.split(":")[1];
+  }
 }
 
-// const router = new RouteNode();
-// router.on("//users//some", HTTP_METHODS.GET, () => {});
-// router.on("/users/some", HTTP_METHODS.GET, () => {});
+const router = new RouteNode({ ignoreDuplicateSlashes: true });
 
-//console.dir(router, { depth: null });
+// router.on("//users//some", HTTP_METHODS.GET, () => {});
+//router.on("/users/some", HTTP_METHODS.GET, () => {});
+router.on("/:id", HTTP_METHODS.GET, [() => {}]);
+router.on("/*", HTTP_METHODS.GET, [() => {}]);
+
+console.dir(router, { depth: null });
