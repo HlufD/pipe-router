8848e9dc7d23ab7a1c1aa922a7583ac707a31ef1
Hluf Abebe <hlufabebe2015@gmail.com>
2026-01-28 21:48:37 +0300

    usese the middleware in the app example

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
 import { PipeRouter } from "./core/router";
 import { PipeServer } from "./core/server";
+import { NextFunction } from "./types/middleware";
 import { Request } from "./types/request";
 import { Response } from "./types/response";
 
-const router = new PipeRouter()
+const router = new PipeRouter();
 const server = new PipeServer(router);
 
-router.get("/", (req: Request, res: Response) => {
-    res.end("hello world")
-})
+router.use((req: Request, res: Response, next: NextFunction) => {
+  console.log("This runs first");
+  next();
+});
+
+router.get("/", async (req: Request, res: Response) => {
+  console.log("this runs second");
+  res.end("hello world");
+});
 
 server.listen(3000, () => {
-    console.log("Server is running on port: 3000")
-})
-
+  console.log("Server is running on port: 3000");
+});
