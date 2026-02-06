647cfca4ca1bffe88a7d7273b45aca1e7b1920bb
Hluf Abebe <hlufabebe2015@gmail.com>
2026-02-06 10:06:24 +0300

    refactor: comment out unused router definitions and improve code clarity

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,52 @@
 import { PipeRouter } from "./core/router";
 
-const router = new PipeRouter();
-const router2 = new PipeRouter();
+// const router = new PipeRouter();
+// const router2 = new PipeRouter();
 
-router.get("/users", () => {
-  console.log("/users");
-});
+// router.get("/users", () => {
+//   console.log("/users");
+// });
 
-router.get("/users/:id", () => {
-  console.log("/users/:id");
-});
+// router.get("/users/:id", () => {
+//   console.log("/users/:id");
+// });
 
-router2.use("/api/v1", router);
-router2.use("/api/v2", router);
+// router2.use("/api/v1", router);
+// router2.use("/api/v2", router);
 
-router.get("/users/:id/*", () => {
-  console.log("/users/:id");
-});
+// router.get("/users/:id/*", () => {
+//   console.log("/users/:id");
+// });
 
-// router
-//   .route("/some/:id")
-//   .get(() => {})
-//   .delete(() => {})
-//   .patch(() => {});
+// // router
+// //   .route("/some/:id")
+// //   .get(() => {})
+// //   .delete(() => {})
+// //   .patch(() => {});
 
-// console.dir(router, { depth: null });
-// console.log("-----------------------------");
-// console.dir(router2, { depth: null });
+// // console.dir(router, { depth: null });
+// // console.log("-----------------------------");
+// // console.dir(router2, { depth: null });
 
-console.log(router2.collectRoutes(router2));
-console.log(router2.collectRoutes(router));
+// console.log(router2.collectRoutes(router2));
+// console.log(router
+// 2.collectRoutes(router));
+
+const users = new PipeRouter();
+const admin = new PipeRouter();
+const app = new PipeRouter();
+
+users.get("/users", () => {});
+admin.get("/admin", () => {});
+
+users.use("/admin", admin);
+app.use("/api", users);
+
+// console.log("-----------USERS--------------");
+// console.dir(users, { depth: null });
+// console.log("----------ADMIN------------------");
+// console.dir(admin, { depth: null });
+console.log("----------APP--------------");
+console.dir(app, { depth: null });
+
+console.log(app.collectRoutes(app));
