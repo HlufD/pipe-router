7bc1e0f7aa5230b0e09c5c07475c6724a18e067e
Hluf Abebe <hlufabebe2015@gmail.com>
2026-02-11 16:16:44 +0300

    refactor: update user route response and adjust middleware execution in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,14 @@ import { Request } from "./core/request";
 import { Response } from "./core/response";
 import { PipeRouter } from "./core/router";
 import { PipeServer } from "./core/server";
+import { NextFunction } from "./types/middleware";
 
 export { PipeServer } from "./core/server";
 
 const userRouter = new PipeRouter();
 
 userRouter.get("/users", (eq: Request, res: Response) => {
-  res.json({ users: [{ name: "Hluf" }] });
+  res.json({ users: [{ name: "John" }] });
 });
 
 userRouter.get("/users/:id", (req: Request, res: Response) => {
@@ -17,10 +18,17 @@ userRouter.get("/users/:id", (req: Request, res: Response) => {
 
 const app = new PipeServer();
 
-app.use("/api", userRouter); // route mounting
-
-app.listen(3001, () => {
-  console.log("server is running on port 3000");
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log("this is from the middleware.");
+  next();
 });
 
-app.use("/some", (req: Request, res: Response) => {});
+app.use("/api", userRouter);
+
+app.use("/some", (req: Request, res: Response, next: NextFunction) => {});
+
+app.listen(3001, () => {
+  console.log("server is running on port 3001");
+});
+
+console.dir(app, { depth: null });
