65822cc51d5943e85f7297605a955209c912a8bf
Hluf Abebe <hlufabebe2015@gmail.com>
2026-02-20 20:40:22 +0300

    add cookie method helpers

---
+++ src/types/cookie-options.d.ts
@@ -0,0 +1,10 @@
+interface CookieOptions {
+  domain?: string;
+  encode?: (val: string) => string;
+  expires?: Date;
+  httpOnly?: boolean;
+  maxAge?: number;
+  path?: string;
+  secure?: boolean;
+  sameSite?: boolean | "lax" | "strict" | "none";
+}
---
+++ src/utils/helpers.ts
@@ -0,0 +1,41 @@
+function serializeCookie(
+  name: string,
+  value: string,
+  options: CookieOptions = {},
+) {
+  const encode = options.encode || encodeURIComponent;
+  let cookie = `${name}=${encode(value)}`;
+
+  if (options.maxAge != null) {
+    const maxAge = Math.floor(options.maxAge / 1000);
+    cookie += `; Max-Age=${maxAge}`;
+    cookie += `; Expires=${new Date(Date.now() + options.maxAge).toUTCString()}`;
+  }
+
+  if (options.domain) cookie += `; Domain=${options.domain}`;
+
+  if (options.path) cookie += `; Path=${options.path}`;
+  else cookie += `; Path=/`;
+
+  if (options.expires) cookie += `; Expires=${options.expires.toUTCString()}`;
+
+  if (options.httpOnly) cookie += `; HttpOnly`;
+
+  if (options.secure) cookie += `; Secure`;
+
+  if (options.sameSite) {
+    const sameSite =
+      options.sameSite === true
+        ? "Strict"
+        : typeof options.sameSite === "string"
+          ? options.sameSite.charAt(0).toUpperCase() +
+            options.sameSite.slice(1).toLowerCase()
+          : undefined;
+
+    if (sameSite) cookie += `; SameSite=${sameSite}`;
+  }
+
+  return cookie;
+}
+
+export { serializeCookie };
